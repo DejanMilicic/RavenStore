8c9ef9f7cf8d490f60aae015818f42775dba84d4
Dejan Milicic <dejan.milicic@gmail.com>
2021-07-13 14:17:26 +0200

    Problem details middleware

--- src/RavenStore/Startup.cs
+++ src/RavenStore/Startup.cs
@@ -5,9 +5,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net.Http;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
+using Hellang.Middleware.ProblemDetails;
 using MediatR;
 using Microsoft.Extensions.Configuration;
 using Raven.Client;
@@ -28,7 +31,9 @@ namespace RavenStore
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services
+                .AddProblemDetails(ConfigureProblemDetails)
+                .AddControllers();
 
             services.AddMediatR(typeof(Startup));
 
@@ -50,9 +55,26 @@ namespace RavenStore
             });
         }
 
+        private void ConfigureProblemDetails(ProblemDetailsOptions options)
+        {
+            options.Rethrow<NotSupportedException>();
+
+            options.MapToStatusCode<NotImplementedException>(StatusCodes.Status501NotImplemented);
+            options.MapToStatusCode<ValidationException>(StatusCodes.Status400BadRequest);
+            options.MapToStatusCode<KeyNotFoundException>(StatusCodes.Status404NotFound);
+
+            options.MapToStatusCode<HttpRequestException>(StatusCodes.Status503ServiceUnavailable);
+
+            // Because exceptions are handled polymorphically, this will act as a "catch all" mapping, which is why it's added last.
+            // If an exception other than NotImplementedException and HttpRequestException is thrown, this will handle it.
+            options.MapToStatusCode<Exception>(StatusCodes.Status500InternalServerError);
+        }
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseProblemDetails();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
