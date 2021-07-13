a09e66200e8fdee5ed5da810520534f42e22f9a9
Dejan Milicic <dejan.milicic@gmail.com>
2021-07-13 16:03:53 +0200

    create product - invalid price

---
+++ src/Specs/Features/Products/CreateProduct/CreateProduct_InvalidPrice.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Net.Http;
+using FluentAssertions;
+using Raven.Client.Documents.Session;
+using RavenStore.Models;
+using Specs.Infrastructure;
+using Xunit;
+
+namespace Specs.Features.Products.CreateProduct
+{
+    [Trait("Create Product", "Invalid Price")]
+    public class CreateProduct_InvalidPrice : Fixture
+    {
+        private readonly HttpResponseMessage _response;
+        private readonly dynamic _newProduct;
+
+        public CreateProduct_InvalidPrice()
+        {
+            _newProduct = new
+            {
+                Name = "Apple",
+                Price = -19.99
+            };
+
+            _response = TestServer.CreateClient().PostAsync("/product", Serialize(_newProduct)).Result;
+
+            WaitForUserToContinueTheTest(Store);
+        }
+
+        [Fact(DisplayName = "1. Status 400 is returned")]
+        public void Status400Returned()
+        {
+            _response.StatusCode.Should().Be(400);
+        }
+
+        [Fact(DisplayName = "2. No products is created in the database")]
+        public void NoProductsCreated()
+        {
+            Store.OpenSession().Query<Product>().Statistics(out QueryStatistics stats).ToList();
+
+            stats.TotalResults.Should().Be(0);
+        }
+    }
+}
