440b96059187319702951de5dfe8a1f494c86dd5
Dejan Milicic <dejan.milicic@gmail.com>
2021-07-13 18:18:01 +0200

    get product - empty name

---
+++ src/Specs/Features/Products/GetProduct/GetProduct_EmptyName.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Net.Http;
+using FluentAssertions;
+using Raven.Client.Documents.Session;
+using RavenStore.Models;
+using Specs.Infrastructure;
+using Xunit;
+
+namespace Specs.Features.Products.GetProduct
+{
+    [Trait("Get Product", "Empty Name")]
+    public class GetProduct_EmptyName : Fixture
+    {
+        private readonly HttpResponseMessage _response;
+        private readonly Product _product;
+        private readonly Product _fetchedProduct;
+
+        public GetProduct_EmptyName()
+        {
+            _product = Generate.Product();
+
+            using (var session = Store.OpenSession())
+            {
+                session.Advanced.WaitForIndexesAfterSaveChanges();
+                session.Store(_product);
+                session.SaveChanges();
+            }
+
+            _response = TestServer.CreateClient().GetAsync($"/product?name=").Result;
+            _fetchedProduct = Deserialize<Product>(_response);
+
+            WaitForUserToContinueTheTest(Store);
+        }
+
+        [Fact(DisplayName = "1. Status 400 is returned")]
+        public void Status400Returned()
+        {
+            _response.StatusCode.Should().Be(400);
+        }
+
+        [Fact(DisplayName = "2. No products returned")]
+        public void FetchedProductIsOkay()
+        {
+            _fetchedProduct.Should().BeNull();
+        }
+    }
+}
