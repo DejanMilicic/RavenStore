4d95de7ba3448cdecd25de7f30e02a76bed137a7
Dejan Milicic <dejan.milicic@gmail.com>
2021-07-13 12:27:25 +0200

    set up fixture

---
+++ src/Specs/Infrastructure/Fixture.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Indexes;
+using Raven.TestDriver;
+using RavenStore;
+using Xunit;
+
+namespace Specs.Infrastructure
+{
+    public class Fixture : RavenTestDriver, IClassFixture<WebApplicationFactory<Startup>>
+    {
+        protected readonly IDocumentStore Store;
+        protected readonly TestServer TestServer;
+
+        public Fixture()
+        {
+            Store = this.GetDocumentStore();
+            IndexCreation.CreateIndexes(typeof(Startup).Assembly, Store);
+
+            TestServer = SetupHost().GetTestServer();
+        }
+
+        public IHost SetupHost()
+        {
+            return new HostBuilder()
+                .ConfigureWebHost(webBuilder =>
+                {
+                    webBuilder.UseStartup<RavenStore.Startup>();
+                    webBuilder
+                        .UseTestServer()
+                        .ConfigureTestServices(collection =>
+                        {
+                            collection.AddSingleton<IDocumentStore>(Store);
+                        });
+                })
+                .StartAsync().Result;
+        }
+    }
+}
