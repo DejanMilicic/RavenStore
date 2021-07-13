00627bc21ec1f754cd5a0203f19fa380d55ff226
Dejan Milicic <dejan.milicic@gmail.com>
2021-07-13 10:58:29 +0200

    set up sample controller

---
+++ src/RavenStore/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RavenStore.Controllers
+{
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("product")]
+        public async Task<string> Get() => "Here is your product";
+    }
+}
